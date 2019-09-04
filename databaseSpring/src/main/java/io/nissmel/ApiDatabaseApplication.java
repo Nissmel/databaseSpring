e53528467f6af9345710f208da939d4a68d4c15e
Łukasz <ukasz@192.168.0.67>
2019-09-04 14:46:11 +0200

    Database connection

---
+++ databaseSpring/src/main/java/io/nissmel/ApiDatabaseApplication.java
@@ -0,0 +1,13 @@
+package io.nissmel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDatabaseApplication.class, args);
+	}
+
+}
---
+++ databaseSpring/src/test/java/io/nissmel/ApiDatabaseApplicationTests.java
@@ -0,0 +1,16 @@
+package io.nissmel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiDatabaseApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
